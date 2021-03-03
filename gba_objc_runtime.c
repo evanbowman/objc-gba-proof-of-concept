3dfbd989c8ba7b538bb128c05bd73ea93e9b8bba
evanbowman <ebowman@bu.edu>
2021-03-03 11:00:07 -0500

    debugging message lookup

--- gba_objc_runtime.c
+++ gba_objc_runtime.c
@@ -4,6 +4,8 @@
 #include "method.h"
 #include <string.h>
 
+#include <stdio.h>
+
 
 enum {
     CLASS_TABLE_SIZE = 100
@@ -43,6 +45,21 @@ id __objc_free_instance(id self)
 }
 
 
+static int sel_is_equal(SEL sel_1, SEL sel_2)
+{
+    if (sel_1 == 0 || sel_2 == 0) {
+        return sel_1 == sel_2;
+    }
+
+    if (sel_1->name == sel_2->name) {
+        return 1;
+    }
+
+    // FIXME...
+    return 1;
+}
+
+
 void* objc_load_method(struct objc_class_gsv1* class, SEL selector)
 {
     struct objc_method_list_gcc* methods = class->methods;
@@ -54,9 +71,10 @@ void* objc_load_method(struct objc_class_gsv1* class, SEL selector)
     for (int i = 0; i < methods->count; ++i) {
         struct objc_method_gcc* method = &methods->methods[i];
 
+        // printf("%s %s\n", selector->types, method->selector->types);
+
         // FIXME: this comparison doesn't work correctly yet!
-        if (((struct objc_selector*)method->selector)->index ==
-            ((struct objc_selector*)selector)->index) {
+        if (sel_is_equal(method->selector, selector)) {
             return method->imp;
         }
         // FIXME
@@ -66,9 +84,36 @@ void* objc_load_method(struct objc_class_gsv1* class, SEL selector)
 }
 
 
+void debug_walk_class(struct objc_class_gsv1* class, int depth)
+{
+    printf("name: %s, info: %zu, inst_size: %zu, abi: %zu\n",
+           class->name,
+           class->info,
+           class->instance_size,
+           class->abi_version);
+
+    if (class->methods) {
+        struct objc_method_list_gcc* methods = class->methods;
+
+        for (int i = 0; i < methods->count; ++i) {
+            struct objc_method_gcc* method = &methods->methods[i];
+            printf("method: %s %zu\n",
+                   method->types ? method->types : "no types",
+                   method->selector->index);
+        }
+    }
+
+    if (class->isa && depth < 2) {
+        debug_walk_class((struct objc_class_gsv1*)class->isa, depth + 1);
+    }
+}
+
+
 id objc_msg_lookup(id receiver, SEL selector)
 {
     Class c = receiver->class_pointer;
+
+    debug_walk_class((struct objc_class_gsv1*)receiver, 0);
 
     // This is super lazy. We should be correctly doing message dispatch by
     // walking up the class hierarchy.
@@ -97,12 +142,17 @@ void __objc_exec_class(struct objc_module_abi_8* module)
                     // to the user?
     }
 
+    while (symbols->selector_count) ;
+
     void* defs = symbols->definitions;
 
     for (int i = 0; i < symbols->class_count; ++i) {
         objc_load_class(defs);
         ++defs;
     }
+
+    // TODO: load categories
+    while (symbols->category_count) ;
 
     for (int i = 0; i < symbols->category_count; ++i) {
         ++defs;
--- main.m
+++ main.m
@@ -1,43 +1,50 @@
 #import "Object.h"
 #include <stdio.h>
-#include </opt/devkitpro/libtonc/include/tonc_tte.h>
-#include </opt/devkitpro/libtonc/include/tonc_video.h>
-#include </opt/devkitpro/libtonc/include/tonc_irq.h>
+// #include </opt/devkitpro/libtonc/include/tonc_tte.h>
+// #include </opt/devkitpro/libtonc/include/tonc_video.h>
+// #include </opt/devkitpro/libtonc/include/tonc_irq.h>
 
 
 @interface HelloWorld:Object {
 }
 +(void) hello: (const char*) param;
++(void) world: (const char*) param;
 @end
 
 
 @implementation HelloWorld
 +(void) hello: (const char*) param {
 
-    irq_init(NULL);
-    irq_add(II_VBLANK, NULL);
-    REG_DISPCNT= DCNT_MODE0 | DCNT_BG0;
+    // irq_init(NULL);
+    // irq_add(II_VBLANK, NULL);
+    // REG_DISPCNT= DCNT_MODE0 | DCNT_BG0;
 
-    tte_init_se(
-        0,
-        BG_CBB(0)|BG_SBB(31),
-        0,
-        CLR_YELLOW,
-        14,
-        NULL,
-        NULL);
+    // tte_init_se(
+    //     0,
+    //     BG_CBB(0)|BG_SBB(31),
+    //     0,
+    //     CLR_YELLOW,
+    //     14,
+    //     NULL,
+    //     NULL);
 
-    pal_bg_bank[1][15]= CLR_RED;
-    pal_bg_bank[2][15]= CLR_GREEN;
-    pal_bg_bank[3][15]= CLR_BLUE;
-    pal_bg_bank[4][15]= CLR_WHITE;
-    pal_bg_bank[5][15]= CLR_MAG;
-    pal_bg_bank[4][14]= CLR_GRAY;
+    // pal_bg_bank[1][15]= CLR_RED;
+    // pal_bg_bank[2][15]= CLR_GREEN;
+    // pal_bg_bank[3][15]= CLR_BLUE;
+    // pal_bg_bank[4][15]= CLR_WHITE;
+    // pal_bg_bank[5][15]= CLR_MAG;
+    // pal_bg_bank[4][14]= CLR_GRAY;
 
-    tte_write(" #{cx:0x1000}Hello from Objc\n");
-    tte_write(param);
+    // tte_write(" #{cx:0x1000}Hello from Objc\n");
+    // tte_write(param);
 
-    key_wait_till_hit(KEY_ANY);
+    // key_wait_till_hit(KEY_ANY);
+
+    printf("%s\n", param);
+}
+
++(void) world: (const char*) param {
+    printf("hmmm %s\n", param);
 }
 @end
 
